c3a478a32ddf485d26eb2e12924ba9c11c322cfd
vycmt <cthuyvy@yahoo.com.vn>
2016-05-26 13:14:03 +0700

    Update changes
    - Update 2 more zip file
    - Update DTO demo

---
+++ WebApp/food-web/src/main/java/com/vtth/food/dto/CategoryDTO.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.vtth.food.dto;
+
+import java.io.Serializable;
+
+/**
+ * @author SONY
+ *
+ */
+public class CategoryDTO implements Serializable {
+
+}
--- WebApp/food-web/src/main/java/com/vtth/food/dto/FoodDTO.java
+++ WebApp/food-web/src/main/java/com/vtth/food/dto/FoodDTO.java
@@ -1,5 +1,33 @@
 package com.vtth.food.dto;
 
-public class FoodDTO {
+import java.io.Serializable;
 
+import com.vtth.food.entity.TblFooddetail;
+
+// Tạo DTO
+// Nhớ phải implement Serializable, trong Entity có bao nhiêu field thì bên này có bấy nhiêu
+//field trừ private TblFooddetail tblFooddetail; 
+//Tạo contrustuctor, và get,set cho từng field
+
+public class FoodDTO implements Serializable {
+        
+    private String foodName;
+    
+    
+    /**
+     * Constructor (bắt buộc phải có)
+     */
+    public FoodDTO() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public String getFoodName() {
+        return foodName;
+    }
+
+    public void setFoodName(String foodName) {
+        this.foodName = foodName;
+    }
+    
+    
 }
