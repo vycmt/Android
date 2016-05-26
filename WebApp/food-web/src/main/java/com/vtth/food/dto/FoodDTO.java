f8dc8f7a8de958c8c4b93a10a42b200b74ebd943
TuanMHV <mhvtuan93@gmail.com>
2016-05-26 16:21:45 +0700

    Tạo DTO
    FoodDetailDTO

--- WebApp/food-web/src/main/java/com/vtth/food/dto/FoodDTO.java
+++ WebApp/food-web/src/main/java/com/vtth/food/dto/FoodDTO.java
@@ -2,6 +2,7 @@ package com.vtth.food.dto;
 
 import java.io.Serializable;
 
+import com.vtth.food.entity.TblCategory;
 import com.vtth.food.entity.TblFooddetail;
 
 // Tạo DTO
@@ -11,14 +12,33 @@ import com.vtth.food.entity.TblFooddetail;
 
 public class FoodDTO implements Serializable {
         
+    private int foodId; 
     private String foodName;
-    
-    
-    /**
-     * Constructor (bắt buộc phải có)
-     */
+    private String description;
+    private String listMaterial;
+    private String images;
+    private int visitNum;
+      
     public FoodDTO() {
         // TODO Auto-generated constructor stub
+    }
+
+    public FoodDTO(int foodId, String foodName, String description, String listMaterial, String images, int visitNum) {
+        super();
+        this.foodId = foodId;
+        this.foodName = foodName;
+        this.description = description;
+        this.listMaterial = listMaterial;
+        this.images = images;
+        this.visitNum = visitNum;
+    }
+
+    public int getFoodId() {
+        return foodId;
+    }
+
+    public void setFoodId(int foodId) {
+        this.foodId = foodId;
     }
 
     public String getFoodName() {
@@ -28,6 +48,37 @@ public class FoodDTO implements Serializable {
     public void setFoodName(String foodName) {
         this.foodName = foodName;
     }
-    
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getListMaterial() {
+        return listMaterial;
+    }
+
+    public void setListMaterial(String listMaterial) {
+        this.listMaterial = listMaterial;
+    }
+
+    public String getImages() {
+        return images;
+    }
+
+    public void setImages(String images) {
+        this.images = images;
+    }
+
+    public int getVisitNum() {
+        return visitNum;
+    }
+
+    public void setVisitNum(int visitNum) {
+        this.visitNum = visitNum;
+    }
     
 }
---
+++ WebApp/food-web/src/main/java/com/vtth/food/dto/FooddetailDTO.java
@@ -0,0 +1,82 @@
+/**
+ * Licensed to MKS Group under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Open-Ones Group licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a
+ * copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.vtth.food.dto;
+
+import com.vtth.food.entity.TblFood;
+
+/**
+ * @author MHVTu
+ *
+ */
+public class FooddetailDTO {
+
+    private int foodId;
+    private String materialDetail;
+    private String tutorial;
+    private String source;
+    
+    public FooddetailDTO() {
+        // TODO Auto-generated constructor stub
+    }
+
+    
+    
+    public FooddetailDTO(int foodId, String materialDetail, String tutorial, String source) {
+        super();
+        this.foodId = foodId;
+        this.materialDetail = materialDetail;
+        this.tutorial = tutorial;
+        this.source = source;
+    }
+
+
+
+    public int getFoodId() {
+        return foodId;
+    }
+
+    public void setFoodId(int foodId) {
+        this.foodId = foodId;
+    }
+
+    public String getMaterialDetail() {
+        return materialDetail;
+    }
+
+    public void setMaterialDetail(String materialDetail) {
+        this.materialDetail = materialDetail;
+    }
+
+    public String getTutorial() {
+        return tutorial;
+    }
+
+    public void setTutorial(String tutorial) {
+        this.tutorial = tutorial;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+    
+}
