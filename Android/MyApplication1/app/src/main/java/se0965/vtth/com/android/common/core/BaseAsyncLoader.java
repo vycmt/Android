315e399e308d757580782860a3e9ac6f59e1d358
vycmt <cthuyvy@yahoo.com.vn>
2016-05-29 22:21:57 +0700

    Test demo

---
+++ Android/MyApplication1/app/src/main/java/se0965/vtth/com/android/common/core/BaseAsyncLoader.java
@@ -0,0 +1,7 @@
+package se0965.vtth.com.android.common.core;
+
+/**
+ * Created by SONY on 5/29/2016.
+ */
+public class BaseAsyncLoader {
+}
--- New folder/MyApplication1/app/src/main/java/se0965/vtth/com/android/slidingtab/SlidingTabLayout.java
+++
@@ -1,7 +0,0 @@
-package se0965.vtth.com.android.slidingtab;
-
-/**
- * Created by SONY on 5/28/2016.
- */
-public class SlidingTabLayout {
-}
