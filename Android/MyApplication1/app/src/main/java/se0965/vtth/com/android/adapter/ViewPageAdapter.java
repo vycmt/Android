36b0ffce113c7cb261f1b1bfc5de6b50b3e2d672
vycmt <cthuyvy@yahoo.com.vn>
2016-06-01 09:54:15 +0700

    Update new file .jsp

--- Android/MyApplication1/app/src/main/java/se0965/vtth/com/android/adapter/ViewPageAdapter.java
+++ Android/MyApplication1/app/src/main/java/se0965/vtth/com/android/adapter/ViewPageAdapter.java
@@ -1,7 +1,31 @@
 package se0965.vtth.com.android.adapter;
 
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
 /**
  * Created by SONY on 5/28/2016.
  */
-public class ViewPageAdapter {
+public class ViewPageAdapter extends BaseAdapter {
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
 }
