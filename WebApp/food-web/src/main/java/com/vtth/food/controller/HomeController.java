1049530628ebab27f1deb3d89305c7354b7f62ab
haivt5 <vuthanhhai1995@gmail.com>
2016-06-06 16:14:11 +0700

    commit login function
    nhờ Vy test giùm đê

--- WebApp/food-web/src/main/java/com/vtth/food/controller/HomeController.java
+++ WebApp/food-web/src/main/java/com/vtth/food/controller/HomeController.java
@@ -3,13 +3,21 @@
  */
 package com.vtth.food.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.cathl.icook.entity.TblFood;
+import com.cathl.icook.entity.TblUser;
 
 /**
  * @author SONY
@@ -23,10 +31,45 @@ public class HomeController {
         return new ModelAndView("index");
     }
     
+   
     
-    @RequestMapping(value="/Admin", method=RequestMethod.GET)
-    public ModelAndView login(Model model, HttpSession session){
-        
-        return new ModelAndView("login");
-    }
+    @RequestMapping(value = "/Admin", method = RequestMethod.POST)
+	public String home(@ModelAttribute("user") TblUser user, Locale locale, Model model, HttpSession session) {
+		TblUser checkUser = userService.checkLogin(user);
+		if (checkUser != null) {
+			session.setAttribute("username", checkUser.getUserName());
+			model.addAttribute("pageheader", "Admin");//set header  at view
+			model.addAttribute("activeTab", "Dashboard");//set active tab at view
+			return "dashboard";
+		}
+		model.addAttribute("error", "wrong");
+		return "login";
+	}
+    
+    @RequestMapping(value = "/Admin", method = RequestMethod.GET)
+   	public ModelAndView login(Model model, HttpSession session) {
+   		if (session.getAttribute("username") != null) {
+
+   			if ((Integer) session.getAttribute("role") == 0) {
+   				model.addAttribute("pageheader", "Admin");//set header  at view
+   				model.addAttribute("activeTab", "Dashboard");//set active tab at view
+   				return "dashboard";
+   			}
+   		} else {
+   			return new ModelAndView("login", "user", new TblUser());
+   		}
+   		return null;
+   	}
+
+	@RequestMapping(value = "/signOut", method = RequestMethod.GET)
+	public String signOut(HttpSession session) {
+		session.removeAttribute("username");
+		session.removeAttribute("role");
+		return "redirect:/";
+	}
+
+	@RequestMapping(value = "/register", method = RequestMethod.GET)
+	public ModelAndView registerPage() {
+		return new ModelAndView("register", "newUser", new TblUser());
+	}
 }
--- WebApp/food-web/src/main/webapp/WEB-INF/pages/login.jsp
+++ WebApp/food-web/src/main/webapp/WEB-INF/pages/login.jsp
@@ -38,18 +38,19 @@
   <!-- Form area -->
   <div class="container">
 
-    <form class="login-form" action="">
+    <spring:form  cssClass="form-horizontal" action="/food-web/Admin"  commandName="user">
+    
       <div class="login-wrap">
         <p class="login-img">
           <i class="icon_lock_alt"></i>
         </p>
         <div class="input-group">
-          <span class="input-group-addon"><i class="icon_profile"></i></span> <input type="text" class="form-control"
-            placeholder="Username" autofocus>
+          <span class="input-group-addon"><i class="icon_profile"></i></span> 
+          <spring:input path="userName" cssClass="form-control" placeholder="Username" value="${requestScope.username}"/>
         </div>
         <div class="input-group">
-          <span class="input-group-addon"><i class="icon_key_alt"></i></span> <input type="password"
-            class="form-control" placeholder="Password">
+          <span class="input-group-addon"><i class="icon_key_alt"></i></span> 
+           <spring:password path="password" cssClass="form-control" placeholder="Password"/>
         </div>
         <label class="checkbox"> <input type="checkbox" value="remember-me"> Remember me <span
           class="pull-right"> <a href="#"> Forgot Password?</a></span>
@@ -57,7 +58,7 @@
         <button class="btn btn-primary btn-lg btn-block" type="submit">Login</button>
         <button class="btn btn-info btn-lg btn-block" type="submit">Signup</button>
       </div>
-    </form>
+    </spring:form>
 
   </div>
 
