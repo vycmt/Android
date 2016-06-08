53ab0d56dbd5c8dfd3e2fb53b656627ab779c2cf
haivt5 <vuthanhhai1995@gmail.com>
2016-06-08 18:15:35 +0700

    fix conflict
    fix create function

---
+++ WebApp/food-web/src/main/java/com/vtth/food/controller/HomeController.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+package com.vtth.food.controller;
+
+import java.util.Locale;
+
+import javax.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import com.vtth.food.entity.TblUser;
+import com.vtth.food.service.FoodService;
+import com.vtth.food.service.UserService;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    UserService userService;
+    @Autowired
+    FoodService foodService;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView homePage() {
+        return new ModelAndView("index");
+    }
+
+    @RequestMapping(value = "/Admin", method = RequestMethod.GET)
+    public ModelAndView login(Model model, HttpSession session) {
+        if (session.getAttribute("username") != null) {
+            model.addAttribute("pageheader", "Admin");// set header at view
+            model.addAttribute("activeTab", "Dashboard");// set active tab at view
+            return new ModelAndView("dashboard");
+        } else {
+            return new ModelAndView("login", "user", new TblUser());
+        }
+    }
+
+    @RequestMapping(value = "/Admin", method = RequestMethod.POST)
+    public String home(@ModelAttribute("user") TblUser user, Locale locale, Model model, HttpSession session) {
+        TblUser checkUser = userService.checkLogin(user);
+        if (checkUser != null) {
+            session.setAttribute("username", checkUser.getUserName());
+            model.addAttribute("pageheader", "Admin");// set header at view
+            model.addAttribute("activeTab", "Dashboard");// set active tab at view
+            return "dashboard";
+        }
+        model.addAttribute("error", "wrong");
+        return "login";
+    }
+    
+    @RequestMapping(value = "/CreatePost", method = RequestMethod.GET)
+	public String createPost(Model model, HttpSession session) {
+		if (session.getAttribute("username") != null) {
+			model.addAttribute("pageheader", "Create New Post");//set header  at view
+			model.addAttribute("activeTab", "CreatePost");//set active tab at view
+			return "create";
+		}
+		return "redirect:/Admin";
+	}
+
+    @RequestMapping(value = "/signOut", method = RequestMethod.GET)
+    public String signOut(HttpSession session) {
+        session.removeAttribute("username");
+        return "redirect:/";
+    }
+
+}
---
+++ WebApp/food-web/src/main/java/com/vtth/food/entity/TblFood.java
@@ -0,0 +1,93 @@
+package com.vtth.food.entity;
+// Generated May 20, 2016 3:45:49 PM by Hibernate Tools 5.1.0.Alpha1
+
+import static javax.persistence.GenerationType.IDENTITY;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tbl_food", catalog = "fooddb")
+public class TblFood implements java.io.Serializable {
+
+    /** . */
+    private static final long serialVersionUID = 432529343386898525L;
+    private Integer foodId;
+    private String foodName;
+    private Integer categoryID;
+    private String description;
+    private String listMaterial;
+    private String images;
+    private Integer visitNum;
+
+    public TblFood(int categoryId, String foodName, String description, String listMaterial) {
+    }
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+
+    @Column(name = "FoodID", unique = true, nullable = false)
+    public Integer getFoodId() {
+        return this.foodId;
+    }
+
+    public void setFoodId(Integer foodId) {
+        this.foodId = foodId;
+    }
+
+    @Column(name = "FoodName", nullable = false, length = 500)
+    public String getFoodName() {
+        return this.foodName;
+    }
+
+    public void setFoodName(String foodName) {
+        this.foodName = foodName;
+    }
+
+    @Column(name = "Description", length = 5000)
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Column(name = "ListMaterial", length = 4000)
+    public String getListMaterial() {
+        return this.listMaterial;
+    }
+
+    public void setListMaterial(String listMaterial) {
+        this.listMaterial = listMaterial;
+    }
+
+    @Column(name = "Images")
+    public String getImages() {
+        return this.images;
+    }
+
+    public void setImages(String images) {
+        this.images = images;
+    }
+
+    @Column(name = "VisitNum")
+    public Integer getVisitNum() {
+        return this.visitNum;
+    }
+
+    public void setVisitNum(Integer visitNum) {
+        this.visitNum = visitNum;
+    }
+
+	public Integer getCategoryID() {
+		return categoryID;
+	}
+
+	public void setCategoryID(Integer categoryID) {
+		this.categoryID = categoryID;
+	}
+
+}
