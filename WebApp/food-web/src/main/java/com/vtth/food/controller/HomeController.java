6bd4f315577be92e2edbc072059686629c7ae1eb
haivt5 <vuthanhhai1995@gmail.com>
2016-06-10 17:07:39 +0700

    change function
    change

--- WebApp/food-web/src/main/java/com/vtth/food/controller/HomeController.java
+++ WebApp/food-web/src/main/java/com/vtth/food/controller/HomeController.java
@@ -3,6 +3,8 @@
  */
 package com.vtth.food.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 import javax.servlet.http.HttpSession;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.vtth.food.entity.TblFood;
 import com.vtth.food.entity.TblUser;
 import com.vtth.food.service.FoodService;
 import com.vtth.food.service.UserService;
@@ -63,8 +67,21 @@ public class HomeController {
             return "createpost";
         }
         return "redirect:/Admin";
-
     }
+    
+    @RequestMapping(value = "/ManagePost", method = RequestMethod.GET)
+	public String managePost(Model model, HttpSession session) {
+		if (session.getAttribute("username") != null && (Integer) session.getAttribute("role") == 0) {
+			List<TblFood> result = new ArrayList<TblFood>();
+			result = foodService.getFood();
+			model.addAttribute("foodPost", result);
+			model.addAttribute("pageheader", "Quản lý bài đăng");//set header  at view
+			model.addAttribute("activeTab", "ManagePost");//set active tab at view
+			return "managepost";
+		}
+		/* model.addAttribute("user", null); */
+		return "redirect:/Admin";
+	}
 
     @RequestMapping(value = "/signOut", method = RequestMethod.GET)
     public String signOut(HttpSession session) {
